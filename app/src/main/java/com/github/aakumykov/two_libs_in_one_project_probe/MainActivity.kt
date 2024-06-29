b4c04aeae8e426ba31ff149829f8df384674feff
Andrey Kumykov <aakumykov@yandex.ru>
2024-06-29 09:46:51 +0300

    Задействовал LibClass1 и LibClass2 в MainActivity.

--- app/src/main/java/com/github/aakumykov/two_libs_in_one_project_probe/MainActivity.kt
+++ app/src/main/java/com/github/aakumykov/two_libs_in_one_project_probe/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.github.aakumykov.probe_lib_1.LibClass1
+import com.github.aakumykov.probe_lib_2.LibClass2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val lc1 = LibClass1()
+        val lc2 = LibClass2()
     }
 }
